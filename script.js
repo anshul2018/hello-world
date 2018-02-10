0278cda9c398d9b63f0b6439f0d1283fc9ab8c41
anshul2018 <36326144+anshul2018@users.noreply.github.com>
2018-02-10 15:32:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-var anshulYOB = 2008;
-console.log(2018-anshulYOB);
+
+//var anshulYOB = 2008;
+//console.log(2018-anshulYOB);
+calcYearOfBirth(10);
+function calcYearOfBirth(age){   
+ var now , year;
+now = new Date();
+//console.log(now);
+ year =   now.getFullYear();
+console.log(year - age   );    
+}
